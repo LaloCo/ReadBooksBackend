ba51dfacaca86a6418f12d5dfa5b8fb7b4f47375
MRZ1 <rzamanbd@gmail.com>
2020-05-25 13:10:16 +0100

    Update Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Post'); // Create a table for 'Book' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
